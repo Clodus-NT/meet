17cfe641edb74ad3a7943f85eca863db14e5a98a
Clodus-NT <clodus.nt@gmail.com>
2022-06-24 15:30:18 -0400

    scenario 3 of show/hide feature is now only half broken instead of completely broken

--- src/features/showHideEventDetails.test.js
+++ src/features/showHideEventDetails.test.js
@@ -39,16 +39,20 @@ defineFeature(feature, test => {
   //Scenario 3
   test('When the user clicks on an expanded event element, the element should collapse.', ({ given, when, then }) => {
     given('an event element is expanded', () => {
-      // AppWrapper = mount(<App />);
-      // AppWrapper.update();
-      // AppWrapper.find('.details-btn').at(0).simulate('click');
-      // expect(AppWrapper.find('.event__Details')).toHaveLength(1);
+      const detailsBtn = AppWrapper.find('.details-btn');
+      const eventDetails = AppWrapper.find('.event__Details');
+      AppWrapper = mount(<App />);
+      AppWrapper.update();
+      detailsBtn.at(0).simulate('click');
+      expect(eventDetails).toHaveLength(1);
     });
     when('the user clicks on an event', () => {
-      // AppWrapper.update();
-      // AppWrapper.find('.details-btn').at(0).simulate('click');
+      AppWrapper.update();
+      AppWrapper.find('.details-btn').at(0).simulate('click');
     });
     then('the event element should collapse.', () => {
+      // const eventDetails = AppWrapper.find('.event__Details');
+      // expect(eventDetails).toHaveLength(0);
       // expect(AppWrapper.find('.event__Details')).toHaveLength(0);
     });
   });
