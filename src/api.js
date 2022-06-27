7033db4a7f7abb5da42f11de927c5013921f56e7
Clodus-NT <clodus.nt@gmail.com>
2022-06-27 17:18:13 -0400

    modified getEvents() in api.js to store events in local storage for offline use in app

--- src/api.js
+++ src/api.js
@@ -29,6 +29,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return data ? JSON.parse(data).events:[];;
+  }
+
   const token = await getAccessToken();
   console.log('getEvents token', token);
   if (token) {
