307c7d5ce588c960ab9363a6745342821c73cbcb
Clodus-NT <clodus.nt@gmail.com>
2022-06-29 12:46:07 -0400

    more console logs

--- src/api.js
+++ src/api.js
@@ -33,6 +33,7 @@ export const getEvents = async () => {
   if (!navigator.onLine) {
     const data = localStorage.getItem('lastEvents');
     NProgress.done();
+    console.log('getEvents offline', data);
     return data ? JSON.parse(data).events:[];;
   }
 
