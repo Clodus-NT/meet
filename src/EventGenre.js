202eaf200c31973c2cb9510341e80d6ad4ab9eec
Clodus-NT <clodus.nt@gmail.com>
2022-07-01 12:22:50 -0400

    added logic to <EventGenre /> component to implement the pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,26 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
-  const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 }
-  ];
+  const [data, setData] = useState([]);
+  const colors = ['#204051', '#84A9AC', '#FABB51', '#3B6978', '#C74B50'];
+
+  useEffect(() => {
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter(({ summary }) => summary.includes(genre)).length;
+        return { name: genre, value };
+      });
+      return data;
+    }
+    setData(() => getData());
+  }, [events]);
 
   return (
     <ResponsiveContainer height={400} >
       <PieChart width={400} height={400}>
         <Pie
           data={data}
-          cx={200}
-          cy={200}
+          cx='50%'
+          cy='50%'
           labelLine={false}
           outerRadius={80}
           fill='#8884d8'
           dataKey='value'
           label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
         >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
         </Pie>
       </PieChart>
     </ResponsiveContainer>
